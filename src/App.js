818f167a1fd6336d1419163573f28df592f45149
SouthernFried-cloud <83118819+SouthernFried-cloud@users.noreply.github.com>
2021-11-13 14:39:58 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -391,17 +391,16 @@ function App() {
             Once you make the purchase, you cannot undo this action. We have set 
             the gas limit to {CONFIG.GAS_LIMIT} for the contract to
             successfully
-            mint your NFT. We recommend that you
+            mint your NFT
+            We recommend that you
             don't lower the
-            gas limit.
-            <s.SpacerSmall>
+            gas limit
             https:////twitter.com/COWZnfts
             https://instagram.com/cowznfts 
             https://dicord.gg/XxMJwAzTXX 
             https://polygonscan.com/address/0x1ff72d298f379c2c2869ba5fc00a61a218f6bbeb
             Please click the banner for NFT calendar.io below. Thanks!
           </s.TextDescription>
-          <s.SpacerSmall />
         </s.Container>
       </s.Container>
     </s.Screen>
